1402166ef40318cb27222984e0c015152a5f45f7
Kudłaty <hubert.suchorowski1@gmail.com>
2023-10-10 19:59:00 +0200

    Added simple navbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,26 @@
 </script>
 
 <body>
-	
+	<nav class="navbar">
+		<div class="nav-logo"><img class="cat" src="/CAT-logo.png" alt="placeholder" /></div>
+	</nav>
 </body>
 
 <style lang="scss">
+	.navbar {
+		position: fixed;
+		top: 0%;
+		height: 8vh;
+		width: 100vw;
+		background-color: #b0742a;
+		.nav-logo {
+			display: grid;
+			place-items: center;
+			height: 8vh;
+			.cat {
+				width: 10vw;
+				height: 5vh;
+			}
+		}
+	}
 </style>
